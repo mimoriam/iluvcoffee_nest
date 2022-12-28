5a7ca3cc142b490865f6bee2a6b52fac125a6ff2
Mimo <mimoriam420@gmail.com>
2022-12-28 15:24:41 +0500

    Commit #2 - Created initial Entity + hooked TypeORM to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_course',
+      autoLoadEntities: true,
+      subscribers: [],
+      migrations: [],
+      logging: false,
+      synchronize: true,
+      cache: {
+        duration: 3000, // 3 seconds
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
