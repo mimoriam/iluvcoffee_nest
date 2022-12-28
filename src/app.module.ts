9fea0abd6e58244f1eaa0c06dbab58a0588296c6
Mimo <mimoriam420@gmail.com>
2022-12-28 19:01:41 +0500

    Commit #10 - Schema Validation + Load TypeORM asynchronously

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,29 +2,37 @@ import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    CoffeesModule,
+    // Config will always be at the top:
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+      }),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      subscribers: [],
-      migrations: [],
-      logging: false,
-      synchronize: true,
-      cache: {
-        duration: 3000, // 3 seconds
-      },
+    CoffeesModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        subscribers: [],
+        migrations: [],
+        logging: false,
+        synchronize: true,
+        cache: {
+          duration: 3000, // 3 seconds
+        },
+      }),
     }),
   ],
   controllers: [],
